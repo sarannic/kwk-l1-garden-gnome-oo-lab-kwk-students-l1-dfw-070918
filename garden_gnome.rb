b0e006879d5fb64868f3142e9b6bde34e7897b36
sarannic <snickershelton11@gmail.com>
2018-07-13 18:31:50 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -57,10 +57,9 @@ class GardenGnome
   def introduce_self 
     return "Hello humans, my name is #{@name}, I am #{@age}, and you'll rue the day you crossed me!"
   end
-
 end
 
-gnome1 = GardenGnome.new()
-gnome2 = GardenGnome.new("Walter the Worst")
-gnome3 = GardenGnome.new("James the Jerk", "3241")
-gnome4 = GardenGnome.new("Alfred the Abhorrent", "579", "true")
+# gnome1 = GardenGnome.new()
+# gnome2 = GardenGnome.new("Walter the Worst")
+# gnome3 = GardenGnome.new("James the Jerk", "3241")
+# gnome4 = GardenGnome.new("Alfred the Abhorrent", "579", "true")
