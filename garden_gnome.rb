a426ad3856fa29eccad38f4b85cd6445dff9dc49
sarannic <snickershelton11@gmail.com>
2018-07-13 18:46:04 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -52,7 +52,7 @@ class GardenGnome
   end 
   
   def introduce_self 
-    return "Hello humans, my name is #{@name}, I am #{@age}, and you'll rue the day you crossed me!"
+    return "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
   end
 end
 
