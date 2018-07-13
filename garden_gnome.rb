2c0deb7ea81108b7093cb86015ec018d15b1df72
sarannic <snickershelton11@gmail.com>
2018-07-13 18:26:53 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -45,7 +45,3 @@ gnome1 = GardenGnome.new()
 gnome2 = GardenGnome.new("Walter the Worst")
 gnome3 = GardenGnome.new("James the Jerk", "3241")
 gnome4 = GardenGnome.new("Alfred the Abhorrent", "579", "true")
-
-puts gnome4.name
-
-
